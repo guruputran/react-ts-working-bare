6f50af5df7c2fff19fe18d5aa9794cadda8c6157
Raj Kumar <gpsmartcodes@gmail.com>
2022-10-21 23:26:51 +0800

    Reactjs tsx bare repo1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h3>This runs with prettier auto in Vscode</h3>
+      <p>
+        Go to View/Coomandplatete. Then choose Prefernces: Open User settings.json and copy:
+        {/* "[typescriptreact]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+      },
+    "[typescript]": {
+        "editor.defaultFormatter": "esbenp.prettier-vscode"
+    }, */}
+      </p>
+    </>
+  );
 }
 
 export default App;
